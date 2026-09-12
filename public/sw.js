010ed31322079df94032d066e5dcdca1f4f8f9ed
futebetesgol <futebetesgol@gmail.com>
2026-09-12 08:59:37 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "cidarank-v6-17-pwa-1";
+const CACHE_VERSION = "cidarank-v6-19-1-ranking-live";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const OFFLINE_URL = "/offline.html";
